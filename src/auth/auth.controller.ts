37e7bdbd6fe5ad0dd3b7474856107f5ed9c0829e
Felipe <felipeicloud11@gmail.com>
2023-08-11 16:11:21 -0300

    feat: add signin and signup functionalities to service

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
 
-@Controller()
-export class AuthController { }
+// Responsible for handling requests
+// /auth
+@Controller('auth')
+export class AuthController {
+  constructor(private authService: AuthService) { }
+
+  // /auth/signup
+  @Post('signup')
+  signUp() {
+    return this.authService.signUp();
+  }
+
+  // /auth/signin
+  @Post('signin')
+  signIn() {
+    return this.authService.signIn();
+  }
+}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,15 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable } from '@nestjs/common';
 
+// Injectable Decorator allows dependency injection, which
+// is a programming technique that makes a class independent
+// of its dependencies
 @Injectable()
-export class AuthService({})
+// Responsible for handling the business logic
+export class AuthService {
+  signUp() {
+    return { message: 'I am signin up!' };
+  }
+  signIn() {
+    return { message: 'I am signin in!' };
+  }
+}
