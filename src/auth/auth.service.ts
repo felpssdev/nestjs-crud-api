eed1c3583ea995f9b7fd354fca88ab38a425fda8
Felipe <felipeicloud11@gmail.com>
2023-08-11 18:53:41 -0300

    feat: signup service funcion

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
 import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 
 // Injectable Decorator allows dependency injection, which
 // is a programming technique that makes a class independent
@@ -15,15 +16,27 @@ export class AuthService {
     const hash = await argon.hash(dto.password);
 
     // Save the new user in the DB
-    const user = await this.prisma.user.create({
-      data: {
-        email: dto.email,
-        hash,
-      },
-    });
+    try {
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash,
+        },
+      });
+
+      delete user.hash;
+
+      return { user };
+    } catch (err) {
+      if (err instanceof PrismaClientKnownRequestError) {
+        if (err.code === 'P2002') {
+          throw new ForbiddenException('Credentials already taken!');
+        }
+      }
+      throw err;
+    }
 
     // Return the saved user
-    return { user };
   }
   signIn() {
     return { message: 'I am signin in!' };
