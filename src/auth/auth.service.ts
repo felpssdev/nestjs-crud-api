cbb15542998c639f0d8ce2b340b8894cdddd0f48
Felipe <felipeicloud11@gmail.com>
2023-08-11 17:49:28 -0300

    feat: add argon2

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
 
 // Injectable Decorator allows dependency injection, which
 // is a programming technique that makes a class independent
@@ -8,7 +9,7 @@ import { PrismaService } from 'src/prisma/prisma.service';
 // Responsible for handling the business logic
 export class AuthService {
   constructor(private prisma: PrismaService) { }
-  signUp() {
+  signUp(dto: AuthDto) {
     return { message: 'I am signin up!' };
   }
   signIn() {
